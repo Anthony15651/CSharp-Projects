6ea8a88a9d0e4ffaa61432a6fd2fa53d1d48d935
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-07-27 13:39:59 -0700

    More Work in C#

---
+++ BasicMath/BasicMath.cs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMath.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = 10;
+            int num2 = 5;
+
+            // Addition
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+
+            // Subtraction
+            int difference = num1 - num2;
+            Console.WriteLine(difference);
+
+            // Multiplication
+            int product = num1 * num2;
+            Console.WriteLine(product);
+
+            // Division
+            int dividend = num1 / num2;
+            Console.WriteLine(dividend);
+
+            // String Concatination
+            string firstName = "Anthony";
+            string lastName = "Garcia";
+            Console.WriteLine(firstName + " " + lastName);
+
+            Console.ReadLine();
+        }
+    }
+}
