2890b46cc7ee4b485e4ee34a295d3e9283b74ded
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-07-31 12:50:38 -0700

    While Statments

--- VariablesAndDataTypes/VariablesAndDataTypes.cs/Program.cs
+++ VariablesAndDataTypes/VariablesAndDataTypes.cs/Program.cs
@@ -23,30 +23,56 @@ namespace VariablesAndDataTypes.cs
             //Console.WriteLine("Your favorite number plus 5 is: " + total);
             //Console.ReadLine();
 
-            bool isStudying = false;
-            byte hoursWorked = 42;
-            sbyte currentTemperature = 23;
-            char randomLetter = 'x';
-            char questionMark = '\u2103';
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemperature = 23;
+            //char randomLetter = 'x';
+            //char questionMark = '\u2103';
 
-            decimal moneyInBank = 100.5m;
+            //decimal moneyInBank = 100.5m;
 
-            double heightInCentimeters = 211.30202092;
+            //double heightInCentimeters = 211.30202092;
 
-            float secondsLeft = 2.62f;
+            //float secondsLeft = 2.62f;
 
-            short temperatureOnMars = -341;
+            //short temperatureOnMars = -341;
 
-            string myName = "Anthony";
+            //string myName = "Anthony";
 
-            int currentAge = 30;
-            string yearsOld = currentAge.ToString();
+            //int currentAge = 30;
+            //string yearsOld = currentAge.ToString();
 
-            bool isRaining = true;
-            string rainingStatus = Convert.ToString(isRaining);
-            Console.WriteLine(rainingStatus);
+            //bool isRaining = true;
+            //string rainingStatus = Convert.ToString(isRaining);
+            //Console.WriteLine(rainingStatus);
+            //Console.ReadLine();
+
+            int day = 4;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
             Console.ReadLine();
-
         }
     }
 }
---
+++ WhileStatements/WhileStatements.cs/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileStatements.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            int guesses = 0;
+
+            while (guesses != 3)
+            {
+                Console.WriteLine("Guess a number!");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                guesses += 1;
+            }
+
+            Console.WriteLine("Good guesses!");
+
+            Console.WriteLine("What number am I thinking of (between 1 - 5)?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool numberGuessed = false;
+
+            do
+            {
+                switch (number)
+                {
+                    case 3:
+                        Console.WriteLine("Correct, 3 was the number I was thinking of!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        numberGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, that was not the number I was thinking of.\nPlease guess again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+            }
+            while (!numberGuessed);
+            Console.ReadLine();
+        }
+    }
+}
