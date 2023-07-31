06b98312fb497b0b363fd3315aa17f16937db471
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-07-31 13:22:40 -0700

    String Practice

---
+++ StringFunctions/StringFunctions.cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Anthony";
+            string quote = "The man said, \"Hello, Anthony\".\nHello on a new line.\n\tHello on a tab.";
+            string filename = @"C:\Users\Anthony";
+
+            bool trueOrfalse = name.Contains("y");
+            trueOrfalse = name.EndsWith("n");
+
+            int length = name.Length;
+
+            Console.WriteLine(length);
+            Console.Read();
+        }
+    }
+}
---
+++ StringsAssignment/StringsAssignment.cs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAssignment.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Concatenates three strings
+            string one = "This is ";
+            string two = "three strings ";
+            string three = "concatinated.";
+
+            Console.WriteLine(one + two + three);
+
+            // Converts string to upper
+            string name = "anthony";
+            Console.WriteLine("Here is the string \"anthony\"");
+            string nameUpper = name.ToUpper();
+            Console.WriteLine("Here is that string with \"ToUpper\" applied:\n" + nameUpper);
+
+            // Creates a Stringbuilder
+            StringBuilder mySB = new StringBuilder();
+            mySB.Append("This will be a paragraph of appended sentences. ");
+            mySB.Append("Usually, a paragraph is five sentences. ");
+            mySB.Append("I've ran out of sentence ideas. ");
+            mySB.Append("This paragraph will only be four sentences instead.");
+            Console.WriteLine(mySB);
+            Console.ReadLine();
+
+        }
+    }
+}
