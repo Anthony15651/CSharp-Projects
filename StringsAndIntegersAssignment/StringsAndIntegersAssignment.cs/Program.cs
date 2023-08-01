012b6c27b127df87fb73a65a21a7b027bbbeb06c
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-08-01 13:55:24 -0700

    Exception Handling Assignment

---
+++ StringsAndIntegersAssignment/StringsAndIntegersAssignment.cs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndIntegersAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int> { 10, 12, 32, 26, 9, 33, 4, 117, 23 };
+            Console.WriteLine("Please enter a number to divide each number in my list by.");
+            {
+                try
+                {
+                    int userInt = Convert.ToInt32(Console.ReadLine());
+                    foreach (int number in intList)
+                    {
+                        int result = number / userInt;
+                        Console.WriteLine(result);
+                    }
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("I'm sorry, but you cannot divide by zero.");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("I'm sorry, but you must enter an integer.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.ReadLine();
+                }
+                
+            }
+            Console.WriteLine("Emerged from block.");
+            Console.ReadLine();
+        }
+    }
+}
