ea71aa4d04f02a8b1fd1ce630581d84338870e1a
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-08-01 16:14:58 -0700

    Calling Method Assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment.cs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleMath myNumber = new SimpleMath();
+            Console.WriteLine("Please input a number, and I will perform some calculations with it:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Calling addition function
+            int userSum = myNumber.Addition(userInput);
+            Console.WriteLine("Your number with 10 added to it is: " + userSum);
+
+            // Calling subtraction function
+            int userDifference = myNumber.Subtraction(userInput);
+            Console.WriteLine("Your number with 10 taken away from it is: " + userDifference);
+
+            // Calling multiplication function
+            int userProduct = myNumber.Multiply(userInput);
+            Console.WriteLine("Your number with 10 multiplied to it is: " + userProduct);
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ CallingMethodsAssignment/CallingMethodsAssignment.cs/SimpleMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment.cs
+{
+    public class SimpleMath
+    {
+        public SimpleMath()
+        {
+
+        }
+        public int NumberValue { get; set; }
+
+        // Addition method
+        public int Addition(int userInput)
+        {
+            return (userInput + 10);
+        }
+
+        // Subtraction method
+        public int Subtraction(int userInput)
+        {
+            return (userInput - 10);
+        }
+
+        // Multiplication method
+        public int Multiply(int userInput)
+        {
+            return (userInput * 10);
+        }
+
+    }
+}
