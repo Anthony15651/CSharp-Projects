f03255078d84df71dc55519c277023f86e67f990
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-08-01 17:40:40 -0700

    Update Program.cs

--- MainMethodAssignment/MainMethodAssignment.cs/Program.cs
+++ MainMethodAssignment/MainMethodAssignment.cs/Program.cs
@@ -12,14 +12,17 @@ namespace MainMethodAssignment.cs
         {
             SimpleMath NumberValue = new SimpleMath();
 
+            // Gets user input, converts it to int
             Console.WriteLine("Please enter an integer: ");
             int userInput = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your number plus 10 equals: " + NumberValue.Addition(userInput));
 
+            // Gets user input, converts it to decimal
             Console.WriteLine("Please enter a decimal: ");
             decimal userInput2 = Convert.ToDecimal(Console.ReadLine());
             Console.WriteLine("Your number minus 10 equals: " + NumberValue.Addition(userInput2));
 
+            // Gets user input, keeps it as a string
             Console.WriteLine("Please enter an integer string: ");
             string userInput3 = Console.ReadLine();
             Console.WriteLine("Your number plus 50 equals: " + NumberValue.Addition(userInput3));
