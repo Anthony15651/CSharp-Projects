d47a2ac40485818cf6f26863fc1c153f207782cd
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-08-02 12:47:47 -0700

    Another Method Assignment

---
+++ MethodClassAssignment/MethodClassAssignment.cs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class
+            SimpleMath NumberValue = new SimpleMath();
+
+            // Getting 2 numbers from the user
+            Console.WriteLine("Please enter a number:");
+            int userValue1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a second number:");
+            int userValue2 = Convert.ToInt32(Console.ReadLine());
+
+            // Calling the method, passing in the two numbers
+            NumberValue.Addition(userValue1, userValue2);
+            Console.ReadLine();
+
+            // Calling the method (specifiying the parameters by name)
+            NumberValue.Addition(x: userValue2, y: userValue1);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment.cs/SimpleMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment.cs
+{
+    public class SimpleMath
+    {
+        public SimpleMath()
+        {
+
+        }
+        public int NumberValue { get; set; }
+
+        // Creating a void method that take two integers as parameters
+        public void Addition(int x, int y)
+        {
+            // Doing math on the first integer
+            int result = x * 5;
+            //Console.WriteLine(result);
+
+            // Displaying the second integer to the screen
+            Console.WriteLine(y);
+            return;
+        }
+
+    }
+}
