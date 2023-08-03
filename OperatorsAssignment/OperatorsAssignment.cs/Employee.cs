48bc3c0914b3b61d125b435031d886c7413588b8
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-08-03 13:29:16 -0700

    OperatorOverloadAssignment

---
+++ OperatorsAssignment/OperatorsAssignment.cs/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment.cs
+{
+    // Creating Employee class
+    public class Employee
+    {
+        // Creating Id, FirstName, and LastName properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overloading the "==" operator
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            bool isEqual = false;
+            if (employee1.Id == employee2.Id)
+            {
+                isEqual = true;
+            }
+            return isEqual;
+        }
+        // Overloading the "!=" operator
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            bool isNotEqual = false;
+            if (employee1.Id != employee2.Id)
+            {
+                isNotEqual = true;
+            }
+            return isNotEqual;
+        }
+
+        //public static Game operator +(Game game, Player player)
+        //{
+        //    game.Players.Add(player);
+        //    return game;
+        //}
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment.cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating two Employee objects and assigning values
+            Employee employee1 = new Employee() { Id = 1, FirstName = "Sample", LastName = "Employee-One" };
+            Employee employee2 = new Employee() { Id = 1, FirstName = "Sample", LastName = "Employee-Two" };
+            bool isEqual = employee1 == employee2;
+            bool isNotEqual = employee1 != employee2;
+
+            // Comparing if the employee IDs are equal
+            Console.WriteLine("Employee 1 ID (#{0}) = Employee 2 ID (#{1}): {2}", employee1.Id, employee2.Id, isEqual);
+            Console.WriteLine("Employee 1 ID (#{0}) != Employee 2 ID (#{1}): {2}", employee1.Id, employee2.Id, isNotEqual);
+
+            Console.ReadLine();
+        }
+    }
+}
