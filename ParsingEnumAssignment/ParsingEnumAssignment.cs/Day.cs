2a9ccae705f2fd594308364709f39fa3f1f22730
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-08-03 16:42:46 -0700

    ParsingAssignment

---
+++ ParsingEnumAssignment/ParsingEnumAssignment.cs/Day.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumAssignment.cs
+{
+    public class Day
+    {
+        // Creating an enum for days of the week
+        public enum DayOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
---
+++ ParsingEnumAssignment/ParsingEnumAssignment.cs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumAssignment.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool tryAgain = true;
+
+            // Prompting user for current day of the week
+            Console.WriteLine("Please enter the current day of the week:");
+
+
+            // Try / Catch block to make sure input is valid
+            while (tryAgain)
+            {
+                try
+                {
+                    DayOfWeek userInput1 = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine());
+                    Console.WriteLine("That day of the week checks out! Today is {0}.\nThanks!", userInput1);
+                    tryAgain = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an actual day of the week (Capitalized).");
+                }
+                finally
+                {
+                    
+                }
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
