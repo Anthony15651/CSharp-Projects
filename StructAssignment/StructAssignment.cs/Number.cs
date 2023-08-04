d9f21c3d95ab92c801852056d498cd12f1082918
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-08-04 11:26:33 -0700

    Struct Practice

---
+++ StructAssignment/StructAssignment.cs/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment.cs
+{
+    // Creating a struct called "Number"
+    public struct Number
+    {
+        // Giving it a property of "Amount"
+        public decimal Amount { get; set; }
+
+    }
+}
---
+++ StructAssignment/StructAssignment.cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating the object and assigning an amount to it
+            Number number = new Number() { Amount = 5 };
+
+            // Printint the amount to the console
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
