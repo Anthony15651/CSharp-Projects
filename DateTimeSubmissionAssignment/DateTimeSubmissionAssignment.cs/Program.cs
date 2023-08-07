e2598b066c74f9c6cccd7ffbb97acaa90ed84f46
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-08-07 12:46:22 -0700

    DateTime Assignment

---
+++ DateTimeSubmissionAssignment/DateTimeSubmissionAssignment.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubmissionAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Printing the current date and time to the console
+            DateTime rightNow = DateTime.Now;
+            Console.WriteLine(rightNow);
+
+            // Getting a number from the user
+            Console.WriteLine("Please enter a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Printing to console the exact time it will be in "userInput" hours
+            TimeSpan timeDifference = new TimeSpan(userInput, 0, 0);
+            DateTime futureTime = rightNow.Add(timeDifference);
+            Console.WriteLine("The time in {0} hours will be: {1}", userInput, futureTime);
+
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/TwentyOne.cs/Dealer.cs
+++ TwentyOne/TwentyOne.cs/Dealer.cs
@@ -20,6 +20,7 @@ namespace TwentyOne.cs
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\antho\OneDrive\Desktop\test.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
                 Deck.Cards.RemoveAt(0);
