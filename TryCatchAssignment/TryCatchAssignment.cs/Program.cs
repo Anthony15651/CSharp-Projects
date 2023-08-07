66477fcb475f56d04d0f9882c4fd3ec68ab1f34b
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-08-07 16:45:13 -0700

    Try/Catch Practice

---
+++ TryCatchAssignment/TryCatchAssignment.cs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime rightNow = DateTime.Now;
+
+            // Asking user for their age
+            Console.WriteLine("Please enter your age.");
+
+            // Try / Catch block for user input
+            try
+            {
+                // Displaying the year the user was born
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput <= 0) throw new InvalidOperationException();
+                DateTime birthYear = rightNow.AddYears(-userInput);
+                DateTime birthYear2 = rightNow.AddYears(-(userInput + 1));
+                Console.WriteLine("If you've already had your birthday this year, your birth year was {0}. Otherwise, your birth year was {1}.", birthYear.Year, birthYear2.Year);
+                Console.ReadLine();
+                return;
+            }
+            // Catch for numbers less than 1
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Your age must be greater than 0.");
+                Console.ReadLine();
+            }
+            // Catch for everything else
+            catch (Exception)
+            {
+                Console.WriteLine("Your input must be a number!");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
--- TwentyOne/TwentyOne.cs/Program.cs
+++ TwentyOne/TwentyOne.cs/Program.cs
@@ -12,12 +12,18 @@ namespace TwentyOne.cs
         {
             const string casinoName = "Grand Hotel and Casino";
 
-            Guid identifier = Guid.NewGuid();
-
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name:", casinoName);
             string playerName = Console.ReadLine();
-            Console.WriteLine("And how much mondy did you bring with you today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+            }
+
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21?", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya" || answer == "yup")
@@ -33,7 +39,20 @@ namespace TwentyOne.cs
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security, kick this person out!");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("An error ocured. Please contact your system administrator.");
+                    }
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing");
