a73a814830e7eb9949e69608561ab5495876c218
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-10-11 12:47:37 -0700

    Finishing Dice Game

--- DecisionLogicPractice/Program.cs
+++ DecisionLogicPractice/Program.cs
@@ -4,10 +4,6 @@ int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
 
-roll1 = 6;
-roll2 = 6;
-roll3 = 6;
-
 int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
@@ -23,10 +19,14 @@ if ((roll1 == roll2) || (roll1 == roll3) || (roll2 == roll3))
     }
 }
 
-if (total >= 15) {
-    Console.WriteLine("You win!");
+if (total >= 16) {
+    Console.WriteLine("You win a new car!");
+} else if (total >= 10) {
+    Console.WriteLine("You win a new laptop!");
+} else if (total == 7) {
+    Console.WriteLine("You win a trip for two!");
 } else {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a kitten!");
 }
 
 
