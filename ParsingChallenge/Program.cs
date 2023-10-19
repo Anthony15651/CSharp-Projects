3e0622bcba43529c0776c836c83158f787535f29
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-10-19 16:39:49 -0700

    Practice sorting arrays

---
+++ ParsingChallenge/Program.cs
@@ -0,0 +1,18 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+// Convert string to an array and sorting it
+string[] orders = orderStream.Split(",");
+Array.Sort(orders);
+
+for (int i = 0; i < orders.Length; i++)
+{
+    char[] orderCharacters = orders[i].ToCharArray();
+    if (orderCharacters.Length != 4)
+    {
+        Console.WriteLine($"{orders[i]}\t - Error");
+    }
+    else
+    {
+        Console.WriteLine(orders[i]);
+    }
+}
