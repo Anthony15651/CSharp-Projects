a53cb612721fe44fa65ebef385f54563331a13fa
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-10-20 12:35:55 -0700

    Setting up invoice challenge

---
+++ AlphanumericVariables/Program.cs
@@ -0,0 +1,20 @@
+﻿// decimal price = 123.45m;
+// int discount = 50;
+// // Using the ":C" after the variable will display the units as currency
+// Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+// decimal measurement = 123456.78912m;
+// // Add numbers after "N" to change how many decimal places show - default (no numbers) is 2
+// Console.WriteLine($"Measurement: {measurement:N4} units");
+
+// // Use "P" to format percentages
+// decimal tax = .36785m;
+// Console.WriteLine($"Tax rate: {tax:P2}");
+
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
