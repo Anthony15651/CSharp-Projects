34a3bc12456ce2704c6d7f71d0105681839bbeea
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-10-20 13:42:03 -0700

    String Practice

--- StringInterpolationChallenge/Program.cs
+++ StringInterpolationChallenge/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+Console.Clear();
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}. Given your current volume, your potential profit would be {newProfit:C}.\n");
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = $"{currentProduct.PadRight(20)}{currentReturn:P}\t{currentProfit:C}\n{newProduct.PadRight(20)}{newReturn:P}\t{newProfit:C}";
+
+// Your logic here
+
+Console.WriteLine(comparisonMessage);
