727b33b9dde962967571308e72afe6893439caff
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-10-24 15:42:25 -0700

    More method practice (overriding methods)

--- ParamatersAndArguments/Program.cs
+++ ParamatersAndArguments/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    DisplayEmail(firstName: corporate[i, 0], lastName: corporate[i, 1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    DisplayEmail(firstName: external[i, 0], lastName: external[i, 1], domain: externalDomain);
+}
+
+void DisplayEmail(string firstName, string lastName, string domain = "contoso.com")
+{
+    string username = firstName.Substring(0, 2).ToLower() + lastName.ToLower();
+    string email = username + "@" + domain;
+    Console.WriteLine($"{email}");
+}
