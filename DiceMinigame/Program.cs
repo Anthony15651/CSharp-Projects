d31188c451a066be43a987766700f8e3f48833c5
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-10-26 14:25:50 -0700

    Starting Petting Zoo project

--- DiceMinigame/Program.cs
+++ DiceMinigame/Program.cs
@@ -1,1 +1,62 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay())
+{
+    PlayGame();
+}
+
+void PlayGame()
+{
+    var play = true;
+
+    while (play)
+    {
+        var target = random.Next(1, 6);
+        var roll = random.Next(1, 6);
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose(roll, target));
+        Console.WriteLine("\nPlay again? (Y/N)");
+
+        play = ShouldPlay();
+    }
+}
+
+bool ShouldPlay()
+{
+    bool inputAccepted = false;
+    while (!inputAccepted)
+    {
+        string userInput = Console.ReadLine().ToLower();
+        if (userInput != null)
+        {
+            if (userInput == "y")
+            {
+                inputAccepted = true;
+                return true;
+            }
+            else if (userInput == "n")
+            {
+                inputAccepted = true;
+                return false;
+            }
+        }
+        
+        Console.WriteLine("Please enter 'Y' or 'N'.");
+    }
+    return false;
+}
+
+string WinOrLose(int roll, int target)
+{
+    if (roll > target)
+    {
+        return ("You win!");
+    }
+    else
+    {
+        return ("You lose.");
+    }
+}
