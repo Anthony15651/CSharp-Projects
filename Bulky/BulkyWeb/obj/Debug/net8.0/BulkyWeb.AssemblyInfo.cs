6007866a595383a4d5cf305b5d8ba5d27cc493bf
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-12-06 13:18:28 -0800

    Added "Create" Page and incorporated validation

--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880d5bda88ffad1e21b712d93fe21378a6316798")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c5c41b2454e81a02227c4882556ee0a5989844d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
