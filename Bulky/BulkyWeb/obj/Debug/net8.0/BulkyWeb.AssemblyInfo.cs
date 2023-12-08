2f5c5226fb91f171a5f349d34030a8bbf73e401a
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-12-07 16:19:54 -0800

    Added toastr notifications

--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6007866a595383a4d5cf305b5d8ba5d27cc493bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd59e930314f31595a87e390f88303f98bdac15")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
